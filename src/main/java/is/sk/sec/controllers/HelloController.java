7fbaebd552e7edf41d420c1401a89cffbfb372ef
Saeed Kayvanfar <skayvanfar.sj@gmail.com>
2021-09-17 19:51:22 +0430

    Forwarding the security context with DelegatingSecurityContextRunnable

--- src/main/java/is/sk/sec/controllers/HelloController.java
+++ src/main/java/is/sk/sec/controllers/HelloController.java
@@ -1,8 +1,16 @@
 package is.sk.sec.controllers;
 
+import org.springframework.scheduling.annotation.Async;
+import org.springframework.security.concurrent.DelegatingSecurityContextCallable;
 import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContext;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.concurrent.Callable;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
 @RestController
 public class HelloController {
@@ -14,5 +22,28 @@ public class HelloController {
         //        Authentication a = context.getAuthentication();
 
         return "Hello, " + a.getName() + "!";
+    }
+
+    @GetMapping("/bye")
+    @Async
+    public void goodbye() {
+        SecurityContext context = SecurityContextHolder.getContext();
+        String username = context.getAuthentication().getName();
+    }
+
+    @GetMapping("/ciao")
+    public String ciao() throws Exception {
+        Callable<String> task = () -> {
+            SecurityContext context = SecurityContextHolder.getContext();
+            return context.getAuthentication().getName();
+        };
+
+        ExecutorService e = Executors.newCachedThreadPool();
+        try {
+            var contextTask = new DelegatingSecurityContextCallable<>(task);
+            return "Ciao, " + e.submit(contextTask).get() + "!";
+        } finally {
+            e.shutdown();
+        }
     }
 }
