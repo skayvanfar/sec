e220592440c6d1e73652d91d69e1a60788b79ca9
Saeed Kayvanfar <skayvanfar.sj@gmail.com>
2021-09-17 19:52:31 +0430

    Forwarding the security context with DelegatingSecurityContextExecutorService

--- src/main/java/is/sk/sec/controllers/HelloController.java
+++ src/main/java/is/sk/sec/controllers/HelloController.java
@@ -2,6 +2,7 @@ package is.sk.sec.controllers;
 
 import org.springframework.scheduling.annotation.Async;
 import org.springframework.security.concurrent.DelegatingSecurityContextCallable;
+import org.springframework.security.concurrent.DelegatingSecurityContextExecutorService;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContext;
 import org.springframework.security.core.context.SecurityContextHolder;
@@ -42,6 +43,22 @@ public class HelloController {
         try {
             var contextTask = new DelegatingSecurityContextCallable<>(task);
             return "Ciao, " + e.submit(contextTask).get() + "!";
+        } finally {
+            e.shutdown();
+        }
+    }
+
+    @GetMapping("/hola")
+    public String hola() throws Exception {
+        Callable<String> task = () -> {
+            SecurityContext context = SecurityContextHolder.getContext();
+            return context.getAuthentication().getName();
+        };
+
+        ExecutorService e = Executors.newCachedThreadPool();
+        e = new DelegatingSecurityContextExecutorService(e);
+        try {
+            return "Hola, " + e.submit(task).get() + "!";
         } finally {
             e.shutdown();
         }
