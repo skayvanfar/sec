d017f0d61257fb3a2f6b930ab9b7cc7a920eef12
Saeed Kayvanfar <skayvanfar.sj@gmail.com>
2021-09-17 13:10:16 +0430

    write a UserDetailsService that has an in-memory list of users.

--- src/main/java/is/sk/sec/config/UserManagementConfig.java
+++ src/main/java/is/sk/sec/config/UserManagementConfig.java
@@ -1,12 +1,14 @@
 package is.sk.sec.config;
 
+import is.sk.sec.model.User;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+
+import java.util.List;
 
 // for authentication
 @Configuration
@@ -14,15 +16,9 @@ public class UserManagementConfig {
 
     @Bean
     public UserDetailsService userDetailsService() {
-        var userDetailsService = new InMemoryUserDetailsManager();
-
-        var user = User.withUsername("john")
-                .password("12345")
-                .authorities("read")
-                .build();
-
-        userDetailsService.createUser(user);
-        return userDetailsService;
+        UserDetails u = new User("john", "12345", "read");
+        List<UserDetails> users = List.of(u);
+        return new InMemoryUserDetailsService(users);
     }
 
     @Bean
