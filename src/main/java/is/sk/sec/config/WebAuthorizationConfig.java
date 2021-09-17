d0b67168f902e921b8bc7e8a83055da7ed87dd0d
Saeed Kayvanfar <skayvanfar.sj@gmail.com>
2021-09-17 19:30:46 +0430

    Implementing custom AuthenticationProvider

--- src/main/java/is/sk/sec/config/WebAuthorizationConfig.java
+++ src/main/java/is/sk/sec/config/WebAuthorizationConfig.java
@@ -1,12 +1,24 @@
 package is.sk.sec.config;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
 // for authorization
 @Configuration
 public class WebAuthorizationConfig extends WebSecurityConfigurerAdapter {
+
+
+    @Autowired
+    private AuthenticationProvider authenticationProvider;
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) {
+        auth.authenticationProvider(authenticationProvider);
+    }
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
