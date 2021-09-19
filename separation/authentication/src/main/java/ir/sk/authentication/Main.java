93e5a51c2fbd8ad1c52f3f50ce78045efcb26a6f
Saeed Kayvanfar <kayvanfar.sj@gmail.com>
2021-09-19 13:45:07 +0430

    authentication module is added.

---
+++ separation/authentication/src/main/java/ir/sk/authentication/Main.java
@@ -0,0 +1,13 @@
+package ir.sk.authentication;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Main {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Main.class, args);
+    }
+
+}
